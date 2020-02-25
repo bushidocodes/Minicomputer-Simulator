4c80bea2f657765bd0f2f861b3f3de9946eb02c5
Sean McBride <spmcbride1201@gmail.com>
2020-02-25 10:54:28 -0500

    increase buffer and troubleshoot

--- src/com/simulator/awesome/Config.java
+++ src/com/simulator/awesome/Config.java
@@ -2,6 +2,6 @@ package com.simulator.awesome;
 
 public class Config {
     public static short WORD_COUNT = 2048;
-    public static short INPUT_IO_BUFFER_SIZE = 32;
-    public static short OUTPUT_IO_BUFFER_SIZE = 32;
+    public static short INPUT_IO_BUFFER_SIZE = 1024;
+    public static short OUTPUT_IO_BUFFER_SIZE = 1024;
 }
